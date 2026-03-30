4ef1792c493f9fe17875f4ad4fb9e6b0ecbe330e
its-PKN-2k4 <phuckhanhnguyen2004@gmail.com>
2026-03-29 21:05:46 -0400

    Add handler for removing follow of a feed

--- handleFeedFollows.go
+++ handleFeedFollows.go
@@ -60,3 +60,24 @@ func printFeedFollow(username, feedname string) {
 	fmt.Printf("* User:          %s\n", username)
 	fmt.Printf("* Feed:          %s\n", feedname)
 }
+
+func handlerRemoveUserFollowForFeed(s *state, cmd command, user database.User) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("Usage: %s <feed_url>", cmd.name)
+	}
+
+	feed, err := s.db.GetFeedByUrl(context.Background(), cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("Couldn't get feed: %w", err)
+	}
+
+	err0 := s.db.RemoveUserFollowForFeed(context.Background(), database.RemoveUserFollowForFeedParams{
+		UserID: user.ID,
+		FeedID: feed.ID,
+	})
+	if err0 != nil {
+		return fmt.Errorf("Error encountered while removing feed follow: %v", err0)
+	}
+	fmt.Print("Successfully remove user follow for provided feed")
+	return nil
+}
--- main.go
+++ main.go
@@ -49,6 +49,7 @@ func main() {
 	cmds.register("feeds", handlerGetFeeds)
 	cmds.register("follow", middlewareLoggedIn(handlerFollow))
 	cmds.register("following", middlewareLoggedIn(handlerListFeedFollows))
+	cmds.register("unfollow", middlewareLoggedIn(handlerRemoveUserFollowForFeed))
 
 	args := os.Args
 	if len(args) < 2 {
